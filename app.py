d2217a3128a09df0c69c42c0469b63985001d257
Kimachuk-Vol <volodymyr.kimachuk.23@pnu.edu.ua>
2025-10-27 18:10:14 +0200

    workinggggggggggggg!!!

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask  
-app = Flask(__name__)    
+from flask import Flask, render_template
 
-@app.route('/')   # URL '/' to be handled by main() route handler
-def main():
-    return 'Hello, world!'
+app = Flask(__name__)
+    
+@app.route('/')
+def resume():
+    return render_template('resume.html', title="Моє Резюме")
 
-@app.route('/homepage') 
-def home():
-    """View for the Home page of your website."""
-    return "This is your homepage :) "
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title="Контактна Форма")
 
-if __name__ == "__main__":
-    app.run()  # Launch built-in web server and run this Flask webapp, debug=True
+if __name__ == '__main__':
+    app.run() # Launch built-in web server and run this Flask webapp, debug=True
