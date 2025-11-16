30105afacfd3f016509417007ebb33c3d0bf8c56
Kimachuk-Vol <volodymyr.kimachuk.23@pnu.edu.ua>
2025-11-16 22:13:45 +0200

    added relationship beetwen users and posts tables

--- app/posts/models.py
+++ app/posts/models.py
@@ -15,28 +15,26 @@ class PostCategory(enum.Enum):
     other = 'other'
 
 class Post(db.Model):
-    """
-    ORM-модель Post
-    """
     __tablename__ = 'posts'
 
-    # Поле id (Integer, Primary Key)
     id = db.Column(db.Integer, primary_key=True)
-    # Поле title (String(150), Not Null)
-    title = db.Column(db.String(150), nullable=False)
-    # Поле content (Text, Not Null)
-    content = db.Column(db.Text, nullable=False)
-    # Поле posted (DateTime, Default = datetime.utcnow)
-    posted = db.Column(db.DateTime, default=datetime.utcnow)
-    # Поле category (Enum)
-    category = db.Column(db.Enum(PostCategory))
-    #Поле is_active (Boolean, Default = True)
-    is_active = db.Column(db.Boolean, default=True)
-    # Поле author (String(20), Default = 'Anonymous')
-    author = db.Column(db.String(20), default='Anonymous')
 
-    def __repr__(self):
-        return f"<Post(id={self.id}, title='{self.title}', content='{self.content}', author='{self.author}')>"
+    title = db.Column(db.String(150), nullable=False)
+
+    content = db.Column(db.Text, nullable=False)
+
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
+
+    category = db.Column(db.Enum(PostCategory))
+
+    is_active = db.Column(db.Boolean, default=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user: Mapped["User"] = relationship(back_populates="posts")
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r})"
 
 class User(db.Model):
     __tablename__ = "users"
